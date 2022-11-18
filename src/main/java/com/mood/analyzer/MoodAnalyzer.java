4f91264ad389976997604d9ea8c3334fd33b63fe
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-11-18 05:59:03 +0530

    Refractored : Previous Test Cases by means of main method constructors

--- src/main/java/com/mood/analyzer/MoodAnalyzer.java
+++ src/main/java/com/mood/analyzer/MoodAnalyzer.java
@@ -2,8 +2,24 @@ package com.mood.analyzer;
 
 public class MoodAnalyzer {
 
-    public String analyseMood(String message) {
+    private String message;
+
+    public MoodAnalyzer() {
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
         if (message.contains("Happy") || message.contains("Any"))
+            return "Happy";
+        else
+            return "Sad";
+    }
+
+    public String checkMood(String mood) {
+        if (mood.equals("Happy"))
             return "Happy";
         else
             return "Sad";
--- src/test/java/com/mood/tester/MoodTester.java
+++ src/test/java/com/mood/tester/MoodTester.java
@@ -7,11 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MoodTester {
 
     MoodAnalyzer obj = new MoodAnalyzer();
+    MoodAnalyzer a = new MoodAnalyzer("I am in Happy mood");
+    MoodAnalyzer b = new MoodAnalyzer("I am in Sad mood");
+    MoodAnalyzer c = new MoodAnalyzer("I am in Any mood");
 
     @Test
     public void testMood() {
-        assertEquals("Happy" , obj.analyseMood("I am in Happy mood"));
-        assertEquals("Sad" , obj.analyseMood("I am in Sad mood"));
-        assertEquals("Happy" , obj.analyseMood("I am in Any mood"));
+        assertEquals("Happy" , a.analyseMood());
+        assertEquals("Sad" , b.analyseMood());
+        assertEquals("Happy" , c.analyseMood());
+    }
+
+    @Test
+    public void testAnotherMood() {
+        assertEquals("Sad" , obj.checkMood("Sad"));
+        assertEquals("Happy" , obj.checkMood("Happy"));
     }
 }
